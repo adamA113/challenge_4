1fe369cf08a2e288e2b423ae492f4e4d1ce34051
Yasir Tobbileh <yasur96@gmail.com>
2020-11-05 15:22:05 +0200

    WTH is this

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,27 @@
 
+class Connect4 extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            rows = 6,
+            cols = 7,
+            selector = selector,
+            isGameOver = false
+
+        };
+    }
+
+    render(){
+        return(
+        
+            
+        )
+
+    }
+
+}
+
+
+
 
---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Connect4 from './client/App.js'
+
+ReactDOM.render(
+    <Connect4 />,
+  document.getElementById('app')
+)
