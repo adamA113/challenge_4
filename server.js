9898595747c0e46ef2ee75d2110c54e63c1c2f99
Yasir Tobbileh <yasur96@gmail.com>
2020-11-05 11:14:48 +0200

    setting the express server

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static(__dirname + '/client'));
+
+app.post('/c4', function (req, res) {
+    console.log(req)
+    
+    
+    // res.end();
+});
+
+app.get('/c4', function (req, res) {
+    // res.render("/");
+});
+
+app.listen(3000, () => {
+    console.log('server is listening ')
+})
+
 
 
